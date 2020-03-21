084645c5102ea941d333288e2a4102e09e2c3932
MelhorQueSVN <36746608+MelhorQueSVN@users.noreply.github.com>
2020-03-21 14:33:16 +0000

    Chamada à API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<div id="app"> 
+  <p> Chamada à <a href="https://openweathermap.org/">OpenWeather</a> API </p>
+  {{ this.info }}
+</div>  
+</template> 
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() { 
+    return { 
+       info: null 
+    }
+  }, 
+  mounted: function() {
+    // o url da chamada tem de ter sempre aquele formato http://... e sempre!! com appid
+    axios
+      .get("http://api.openweathermap.org/data/2.5/uvi/history?appid=2aaf6c39c46c00fc3969d144655bf6c2&lat=37.75&lon=-122.37&start=1498049953&end=1498481991")
+      .then(dados => { 
+        console.log(dados) 
+        this.info = dados
+      })
+      .catch(err => {
+        this.error = err.message;
+      });
   }
 }
 </script>
