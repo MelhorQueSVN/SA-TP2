e39ab711148a324d8aeed0a0fb73f0ec7a6609c6
MelhorQueSVN <36746608+MelhorQueSVN@users.noreply.github.com>
2020-04-10 15:26:20 +0100

    minor changes landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,10 +1,8 @@
 <template>
-<v-layout row wrap>
-  <v-flex xs12 sm12>
   <v-parallax
     dark 
-    height="620" 
-    src="../assets/background-clouds.jpg"
+    height="630" 
+    src="../assets/background-clouds2.jpg"
   >
     <v-row
       align="center"
@@ -12,21 +10,21 @@
     >
       <v-col class="text-center" cols="12">
         <h1 class="display-1 font-weight-thin mb-4">WeatherForecast</h1>
-        <h4 depressed text class="white--text subheading">Apresentação dos dados fornecidos pelo OpenWeather</h4> 
-        <h4 depressed text class="white--text subheading">e previsão através da API da WorldWeatherOnline</h4>
+        <h4 class="display-0 font-weight-thin mb-4 subheading">Apresentação dos dados fornecidos pelo OpenWeather</h4> 
+        <h4 class="display-0 font-weight-thin mb-4 subheading">e previsão através da API da WorldWeatherOnline</h4>
       </v-col>
      <v-spacer></v-spacer> 
      <v-col class="text-center" cols="12">  
-        <h5 depressed text class="white--text subheading">Trabalho desenvolvido por:</h5> 
-        <h5 depressed text class="white--text subheading">João Nunes</h5>  
-        <h5 depressed text class="white--text subheading">Mateus Silva</h5>  
-        <h5 depressed text class="white--text subheading">Rui Miguel</h5> 
+        <h5 class="display-0 font-weight-thin mb-4 subheading">Trabalho desenvolvido por:</h5> 
+        <h5 class="display-0 font-weight-thin mb-4 subheading">João Nunes</h5>  
+        <h5 class="display-0 font-weight-thin mb-4 subheading">Mateus Silva</h5>  
+        <h5 class="display-0 font-weight-thin mb-4 subheading">Rui Miguel</h5> 
      </v-col>  
     </v-row>
   </v-parallax> 
-  </v-flex> 
-</v-layout>
-</template> 
+</template>  
+
+
 
 <script>
 export default {
@@ -35,7 +33,7 @@ export default {
 </script> 
 
 <style>
-.home .main-content-background {
-    display: none;
-}
+  .v-parallax__content {
+    padding: 0px !important;
+  }
 </style>
